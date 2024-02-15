0b0e8f77125de9998c01a626c3bea81ae1bf42a3
Ahhj93 <69793084+Ahhj93@users.noreply.github.com>
2024-02-15 12:02:06 +0100

    :wrench: (mli) add docs

--- src/bin_tree.ml
+++ src/bin_tree.ml
@@ -2,74 +2,74 @@ type 'a bin_tree =
   | Nil
   | Node of 'a * 'a bin_tree * 'a bin_tree
 
-let rec height (b: 'a bin_tree) : int =
-  match b with
+let rec height (t: 'a bin_tree) : int =
+  match t with
   | Nil -> -1
   | Node(_, l, r) -> 1 + max (height l) (height r)
 
-let rec nb_leaf (b: 'a bin_tree) : int =
-  match b with
+let rec nb_leaf (t: 'a bin_tree) : int =
+  match t with
   | Nil -> 0
   | Node(_, Nil, Nil) -> 1
   | Node(_, l, r) -> nb_leaf l + nb_leaf r
 
-let rec nb_internal_nodes (b: 'a bin_tree) : int =
-  match b with
+let rec nb_internal_nodes (t: 'a bin_tree) : int =
+  match t with
   | Nil -> 0
   | Node(_, Nil, Nil) -> 0
   | Node(_, l, r) -> 1 + nb_internal_nodes l + nb_internal_nodes r
 
-let internal_nodes (b: 'a bin_tree) : 'a list =
-  let rec aux (b: 'a bin_tree) (nodes: 'a list) =
-    match b with
+let internal_nodes (t: 'a bin_tree) : 'a list =
+  let rec aux (t: 'a bin_tree) (nodes: 'a list) =
+    match t with
     | Nil | Node(_, Nil, Nil) -> []
     | Node(v, l, r) -> aux l (v :: aux r nodes)
   in
-  aux b []
+  aux t []
 
-let rec search_node (b: 'a bin_tree) (x: 'a) : bool =
-  match b with
+let rec search_node (t: 'a bin_tree) (x: 'a) : bool =
+  match t with
   | Nil -> false
   | Node(v, _, _) when v = x -> true
   | Node(_, l, r) -> search_node l x || search_node r x
 
-let is_symmetric (b: 'a bin_tree) : bool =
-  let rec aux (b: 'a bin_tree) =
-  match b with
+let is_symmetric (t: 'a bin_tree) : bool =
+  let rec aux (t: 'a bin_tree) =
+  match t with
   | Nil -> Nil
   | Node(v, l, r) -> Node(v, aux l, aux r)
   in
-  b = (aux b)
+  t = (aux t)
 
-let rec is_strict (b: 'a bin_tree) : bool =
-  match b with
+let rec is_strict (t: 'a bin_tree) : bool =
+  match t with
   | Nil -> true
   | Node (_, Nil, Nil) -> true
   | Node (_, l, r) -> l <> Nil && r <> Nil && is_strict l && is_strict r
 
-let rec is_right_comb (b: 'a bin_tree) : bool =
-  match b with
+let rec is_right_comb (t: 'a bin_tree) : bool =
+  match t with
   | Nil -> true
   | Node(_, Nil, r) -> is_right_comb r
   | _ -> false
 
-let rec is_left_comb (b: 'a bin_tree) : bool =
-  match b with
+let rec is_left_comb (t: 'a bin_tree) : bool =
+  match t with
   | Nil -> true
   | Node(_, r, Nil) -> is_left_comb r
   | _ -> false
 
-let is_comb (b: 'a bin_tree) : bool =
-  match b with
+let is_comb (t: 'a bin_tree) : bool =
+  match t with
   | Nil -> true
   | Node (_, Nil, Nil) -> true
   | Node (_, Nil, r) -> is_right_comb r
   | Node (_, l, Nil) -> is_left_comb l
   | _ -> false
 
-let is_perfect (b: 'a bin_tree) : bool =
-  let rec aux (b: 'a bin_tree) =
-    match b with
+let is_perfect (t: 'a bin_tree) : bool =
+  let rec aux (t: 'a bin_tree) =
+    match t with
     | Nil -> (true, 0)
     | Node(_, l, r) ->
       let (verif_l, h_l) = aux l in
@@ -78,5 +78,5 @@ let is_perfect (b: 'a bin_tree) : bool =
       let verif = verif_l && verif_r && (h_l = h_r) in
       (verif, max h_l h_r + 1)
   in
-  let (verif, _) = aux b in
+  let (verif, _) = aux t in
   verif
--- src/bin_tree.mli
+++ src/bin_tree.mli
@@ -1,12 +1,34 @@
 type 'a bin_tree = Nil | Node of 'a * 'a bin_tree * 'a bin_tree
+
+(* [height t] returns the height of the tree [t]. *)
 val height : 'a bin_tree -> int
+
+(* [nb_leaf t] returns the number of leaves of the tree [t]. *)
 val nb_leaf : 'a bin_tree -> int
+
+(* [nb_internal_nodes t] returns the number of internal nodes of the tree [t]. *)
 val nb_internal_nodes : 'a bin_tree -> int
+
+(* [internal_nodes t] returns the list of internal nodes of the tree [t]. *)
 val internal_nodes : 'a bin_tree -> 'a list
+
+(* [search_node t x] returns [true] if the tree [t] contains the node [x], [false] otherwise. *)
 val search_node : 'a bin_tree -> 'a -> bool
+
+(* [is_symmetric t] returns [true] if the tree [t] is symmetric, [false] otherwise. *)
 val is_symmetric : 'a bin_tree -> bool
+
+(* [is_strict t] returns [true] if the tree [t] is strict, [false] otherwise. *)
 val is_strict : 'a bin_tree -> bool
+
+(* [is_right_comb t] returns [true] if the tree [t] is a right comb, [false] otherwise. *)
 val is_right_comb : 'a bin_tree -> bool
+
+(* [is_left_comb t] returns [true] if the tree [t] is a left comb, [false] otherwise. *)
 val is_left_comb : 'a bin_tree -> bool
+
+(* [is_comb t] returns [true] if the tree [t] is a comb, [false] otherwise. *)
 val is_comb : 'a bin_tree -> bool
+
+(* [is_perfect t] returns [true] if the tree [t] is perfect, [false] otherwise. *)
 val is_perfect : 'a bin_tree -> bool
